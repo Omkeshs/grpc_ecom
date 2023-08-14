fb6a1a9a252bba36d78cfea62bfc5847ff94bf21
Omkesh Sajjanwar <omkesh0808@gmail.com>
2023-08-14 20:54:28 +0530

    GRPC Server - UpdateProduct signature updated

--- product/grpc/server.go
+++ product/grpc/server.go
@@ -2,24 +2,22 @@ package grpc
 
 import (
 	"context"
-	"fmt"
 	"net"
 
-	product "github.com/Omkeshs/grpc_ecom/grpc/product"
-	// "https://github.com/Omkeshs/grpc_ecom/tree/main/grpc/product"
 	bl "github.com/Omkeshs/grpc_ecom/product/bl"
-	spec "github.com/Omkeshs/grpc_ecom/product/spec"
+	"github.com/Omkeshs/grpc_ecom/product/spec"
+	pb "github.com/Omkeshs/grpc_service/product"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 type server struct {
 	logger    *zap.Logger
 	productbl bl.ProductBL
-	product.UnimplementedProductServiceServer
+	pb.UnimplementedProductServiceServer
 }
 
 func NewProductServer(log *zap.Logger, svc bl.ProductBL) *server {
@@ -40,7 +38,8 @@ func InitGRPCServer(log *zap.Logger, svc bl.ProductBL, port string) error {
 
 	s := grpc.NewServer()
 
-	product.RegisterProductServiceServer(s, productServer)
+	// e-com grpc service
+	pb.RegisterProductServiceServer(s, productServer)
 
 	reflection.Register(s)
 
@@ -55,7 +54,6 @@ func InitGRPCServer(log *zap.Logger, svc bl.ProductBL, port string) error {
 
 func (svc *server) ListProduct(ctx context.Context, in *pb.ListRequest) (*pb.ListProductResponse, error) {
 
-	fmt.Println(" -- ListProduct")
 	req := spec.ProductRequest{}
 	products, err := svc.productbl.ListProduct(ctx, req)
 	if err != nil {
@@ -77,6 +75,27 @@ func (svc *server) ListProduct(ctx context.Context, in *pb.ListRequest) (*pb.Lis
 		}
 		resp.Products[product.ID] = &p
 	}
-
 	return &resp, nil
+}
+
+func (svc *server) UpdateProduct(ctx context.Context, req *pb.UpdateProductRequest) (resp *pb.Empty, err error) {
+
+	svcReqs := []spec.UpdateProduct{}
+
+	for _, ureq := range req.Product {
+		svcReq := spec.UpdateProduct{
+			ID:       ureq.Id,
+			Quantity: ureq.Quantity,
+		}
+		svcReqs = append(svcReqs, svcReq)
+	}
+
+	err = svc.productbl.UpdateProduct(ctx, svcReqs)
+	if err != nil {
+		svc.logger.Sugar().Debug(err.Error())
+		return nil, err
+	}
+
+	resp = &pb.Empty{}
+	return resp, nil
 }
